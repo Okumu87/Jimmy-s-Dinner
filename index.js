10b1817d78d4bdc61054fcf1f8b94c7a00419e40
okumu87 <okumu793@gmail.com>
2026-02-03 23:04:28 +0100

    css htmtl updated

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
 import { menuArray } from './data.js';
 
-const menuDiv = document.getElementById('menu');
-
-let menuHTML = '';
-
-menuArray.forEach(item => {
-    menuHTML += `
-
-      <div class="menu-item">
-            <div class="menu-recipe">
-                <div class="food-img">
-                <img src="./images/${item.image}" alt="${item.name}">
-                </div>
-                <div class="content-menu">
-                <h3>${item.name}</h3>
-                <p>${item.ingredients.join(', ')}</p>
-                <h4>$${item.price}</h4>
-                </div>
-               
-            </div >  
-             <a href="#" class="add-btn"><img src="./images/add-btn.png" alt="Add to order btn"></a>
-          </div>
-    `;
-});
-
-menuDiv.innerHTML = menuHTML;
-    
-
 
